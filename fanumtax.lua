a293e97861b0596aa99b2f6f25cc464e7a9b9f7e
stopbnnmeples <anothercocakovmuhammadali@mail.ru>
2025-02-04 21:30:17 +0300

    Create fanumtax.lua

---
+++ fanumtax.lua
@@ -0,0 +1,445 @@
+--[[
+
+		Gui2Lua™
+		10zOfficial
+		Version 1.0.0
+
+]]
+
+
+-- Instances
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local ScrollingFrame = Instance.new("ScrollingFrame")
+local UIGridLayout = Instance.new("UIGridLayout")
+local TextButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local TextButton_3 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local TextButton_4 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local UICorner_6 = Instance.new("UICorner")
+local TextButton_5 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local TextButton_6 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local TextButton_7 = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local TextButton_8 = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local TextButton_9 = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local TextButton_10 = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local TextButton_11 = Instance.new("TextButton")
+local UICorner_13 = Instance.new("UICorner")
+local TextButton_12 = Instance.new("TextButton")
+local UICorner_14 = Instance.new("UICorner")
+local TextButton_13 = Instance.new("TextButton")
+local UICorner_15 = Instance.new("UICorner")
+local TextButton_14 = Instance.new("TextButton")
+local UICorner_16 = Instance.new("UICorner")
+local TextButton_15 = Instance.new("TextButton")
+local UICorner_17 = Instance.new("UICorner")
+local TextButton_16 = Instance.new("TextButton")
+local UICorner_18 = Instance.new("UICorner")
+local TextButton_17 = Instance.new("TextButton")
+local UICorner_19 = Instance.new("UICorner")
+local ImageLabel = Instance.new("ImageLabel")
+local UICorner_20 = Instance.new("UICorner")
+local UIPadding = Instance.new("UIPadding")
+local UIPadding_2 = Instance.new("UIPadding")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local UICorner_21 = Instance.new("UICorner")
+local UIPadding_3 = Instance.new("UIPadding")
+local ImageLabel_3 = Instance.new("ImageButton")
+local UICorner_22 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_23 = Instance.new("UICorner")
+
+-- Properties
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.ResetOnSpawn = false
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.new(0.207843, 0.207843, 0.207843)
+Frame.BackgroundTransparency = 0.30000001192092896
+Frame.BorderColor3 = Color3.new(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.288908005, 0, 0.272388071, 0)
+Frame.Size = UDim2.new(0, 491, 0, 366)
+
+UICorner.Parent = Frame
+
+ScrollingFrame.Parent = Frame
+ScrollingFrame.Active = true
+ScrollingFrame.BackgroundColor3 = Color3.new(1, 1, 1)
+ScrollingFrame.BackgroundTransparency = 1
+ScrollingFrame.BorderColor3 = Color3.new(0, 0, 0)
+ScrollingFrame.BorderSizePixel = 0
+ScrollingFrame.Size = UDim2.new(0, 491, 0, 364)
+
+UIGridLayout.Parent = ScrollingFrame
+UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIGridLayout.CellSize = UDim2.new(0, 100, 0, 50)
+
+TextButton.Parent = ScrollingFrame
+TextButton.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton.BackgroundTransparency = 0.30000001192092896
+TextButton.BorderColor3 = Color3.new(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Size = UDim2.new(0, 200, 0, 50)
+TextButton.Font = Enum.Font.Unknown
+TextButton.Text = "Infinite Yield"
+TextButton.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton.TextSize = 14
+local function onClick()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end
+TextButton.MouseButton1Click:Connect(onClick)
+
+UICorner_2.Parent = TextButton
+
+TextButton_2.Parent = ScrollingFrame
+TextButton_2.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_2.BackgroundTransparency = 0.30000001192092896
+TextButton_2.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Size = UDim2.new(0, 200, 0, 50)
+TextButton_2.Font = Enum.Font.Unknown
+TextButton_2.Text = "Vexon Hub"
+TextButton_2.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_2.TextSize = 14
+local function onClick0()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/TSB-VexonHub"))()
+end
+TextButton_2.MouseButton1Click:Connect(onClick0)
+
+UICorner_3.Parent = TextButton_2
+
+TextButton_3.Parent = ScrollingFrame
+TextButton_3.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_3.BackgroundTransparency = 0.30000001192092896
+TextButton_3.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Size = UDim2.new(0, 200, 0, 50)
+TextButton_3.Font = Enum.Font.Unknown
+TextButton_3.Text = "MsPaint"
+TextButton_3.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_3.TextSize = 14
+local function onClick1()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/002c19202c9946e6047b0c6e0ad51f84.lua"))()
+end
+TextButton_3.MouseButton1Click:Connect(onClick1)
+
+UICorner_4.Parent = TextButton_3
+
+TextButton_4.Parent = ScrollingFrame
+TextButton_4.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_4.BackgroundTransparency = 0.30000001192092896
+TextButton_4.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_4.BorderSizePixel = 0
+TextButton_4.Size = UDim2.new(0, 200, 0, 50)
+TextButton_4.Font = Enum.Font.Unknown
+TextButton_4.Text = "Alchemy Hub"
+TextButton_4.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_4.TextSize = 14
+local function onClick2()
+    loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
+end
+TextButton_4.MouseButton1Click:Connect(onClick2)
+
+UICorner_5.Parent = TextButton_4
+
+UICorner_6.Parent = TextButton_4
+
+TextButton_5.Parent = ScrollingFrame
+TextButton_5.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_5.BackgroundTransparency = 0.30000001192092896
+TextButton_5.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_5.BorderSizePixel = 0
+TextButton_5.Size = UDim2.new(0, 200, 0, 50)
+TextButton_5.Font = Enum.Font.Unknown
+TextButton_5.Text = "Name Esp"
+TextButton_5.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_5.TextSize = 14
+local function onClick3()
+	loadstring(game:HttpGet("https://pastebin.com/YeLp4WnH"))()
+end
+TextButton_5.MouseButton1Click:Connect(onClick3)
+
+UICorner_7.Parent = TextButton_5
+
+TextButton_6.Parent = ScrollingFrame
+TextButton_6.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_6.BackgroundTransparency = 0.30000001192092896
+TextButton_6.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_6.BorderSizePixel = 0
+TextButton_6.Size = UDim2.new(0, 200, 0, 50)
+TextButton_6.Font = Enum.Font.Unknown
+TextButton_6.Text = "Hitbox Exp."
+TextButton_6.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_6.TextSize = 14
+local function onClick4()
+	loadstring(game:HttpGet("https://pastebin.com/AghFViLe"))()
+end
+TextButton_6.MouseButton1Click:Connect(onClick4)
+
+UICorner_8.Parent = TextButton_6
+
+TextButton_7.Parent = ScrollingFrame
+TextButton_7.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_7.BackgroundTransparency = 0.30000001192092896
+TextButton_7.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_7.BorderSizePixel = 0
+TextButton_7.Size = UDim2.new(0, 200, 0, 50)
+TextButton_7.Font = Enum.Font.Unknown
+TextButton_7.Text = "punkz (R15)"
+TextButton_7.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_7.TextSize = 14
+local function onClick5()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/blackheartedcurse/punkz-Scripts/main/SolaraVersionRagdoll.lua"))()
+end
+TextButton_7.MouseButton1Click:Connect(onClick5)
+
+UICorner_9.Parent = TextButton_7
+
+TextButton_8.Parent = ScrollingFrame
+TextButton_8.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_8.BackgroundTransparency = 0.30000001192092896
+TextButton_8.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_8.BorderSizePixel = 0
+TextButton_8.Size = UDim2.new(0, 200, 0, 50)
+TextButton_8.Font = Enum.Font.Unknown
+TextButton_8.Text = "Bypass Chat"
+TextButton_8.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_8.TextSize = 14
+local function onClick6()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Gazer-Ha/Reimagined/refs/heads/main/Gaze%20bypass'))()
+end
+TextButton_8.MouseButton1Click:Connect(onClick6)
+
+UICorner_10.Parent = TextButton_8
+
+TextButton_9.Parent = ScrollingFrame
+TextButton_9.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_9.BackgroundTransparency = 0.30000001192092896
+TextButton_9.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_9.BorderSizePixel = 0
+TextButton_9.Size = UDim2.new(0, 200, 0, 50)
+TextButton_9.Font = Enum.Font.Unknown
+TextButton_9.Text = "TP Walk"
+TextButton_9.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_9.TextSize = 14
+local function onClick7()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scarlet1837/a/main/Tpwalk"))()
+end
+TextButton_9.MouseButton1Click:Connect(onClick7)
+
+UICorner_11.Parent = TextButton_9
+
+TextButton_10.Parent = ScrollingFrame
+TextButton_10.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_10.BackgroundTransparency = 0.30000001192092896
+TextButton_10.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_10.BorderSizePixel = 0
+TextButton_10.Size = UDim2.new(0, 200, 0, 50)
+TextButton_10.Font = Enum.Font.Unknown
+TextButton_10.Text = "Fake Lag"
+TextButton_10.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_10.TextSize = 14
+local function onClick8()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RENZXW/RENZXW-SCRIPTS/main/fakeLAGRENZXW.txt"))()
+end
+TextButton_10.MouseButton1Click:Connect(onClick8)
+
+UICorner_12.Parent = TextButton_10
+
+TextButton_11.Parent = ScrollingFrame
+TextButton_11.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_11.BackgroundTransparency = 0.30000001192092896
+TextButton_11.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_11.BorderSizePixel = 0
+TextButton_11.Size = UDim2.new(0, 200, 0, 50)
+TextButton_11.Font = Enum.Font.Unknown
+TextButton_11.Text = "Emote Gui"
+TextButton_11.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_11.TextSize = 14
+local function onClick9()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+end
+TextButton_11.MouseButton1Click:Connect(onClick9)
+
+UICorner_13.Parent = TextButton_11
+
+TextButton_12.Parent = ScrollingFrame
+TextButton_12.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_12.BackgroundTransparency = 0.30000001192092896
+TextButton_12.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_12.BorderSizePixel = 0
+TextButton_12.Size = UDim2.new(0, 200, 0, 50)
+TextButton_12.Font = Enum.Font.Unknown
+TextButton_12.Text = "Spider Gui"
+TextButton_12.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_12.TextSize = 14
+local function onClick10()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiderScriptRB/SpiderScriptRBUniversal/refs/heads/main/Protected_Spider.txt"))()
+end
+TextButton_12.MouseButton1Click:Connect(onClick10)
+
+UICorner_14.Parent = TextButton_12
+
+TextButton_13.Parent = ScrollingFrame
+TextButton_13.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_13.BackgroundTransparency = 0.30000001192092896
+TextButton_13.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_13.BorderSizePixel = 0
+TextButton_13.Size = UDim2.new(0, 200, 0, 50)
+TextButton_13.Font = Enum.Font.Unknown
+TextButton_13.Text = "Spy Gui"
+TextButton_13.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_13.TextSize = 14
+local function onClick11()
+    loadstring(game:HttpGet("https://pastebin.com/raw/uPGrjsd6"))()
+end
+TextButton_13.MouseButton1Click:Connect(onClick11)
+
+UICorner_15.Parent = TextButton_13
+
+TextButton_14.Parent = ScrollingFrame
+TextButton_14.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_14.BackgroundTransparency = 0.30000001192092896
+TextButton_14.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_14.BorderSizePixel = 0
+TextButton_14.Size = UDim2.new(0, 200, 0, 50)
+TextButton_14.Font = Enum.Font.Unknown
+TextButton_14.Text = "Chat Draw"
+TextButton_14.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_14.TextSize = 14
+local function onClick12()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AKadminlol/Chatdraw/refs/heads/main/Chattdraw"))()
+end
+TextButton_14.MouseButton1Click:Connect(onClick12)
+
+UICorner_16.Parent = TextButton_14
+
+TextButton_15.Parent = ScrollingFrame
+TextButton_15.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_15.BackgroundTransparency = 0.30000001192092896
+TextButton_15.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_15.BorderSizePixel = 0
+TextButton_15.Size = UDim2.new(0, 200, 0, 50)
+TextButton_15.Font = Enum.Font.Unknown
+TextButton_15.Text = "Quiz Bot"
+TextButton_15.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_15.TextSize = 14
+local function onClick13()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Damian-11/quizbot/master/quizbot.luau"))()
+end
+TextButton_15.MouseButton1Click:Connect(onClick14)
+
+UICorner_17.Parent = TextButton_15
+
+TextButton_16.Parent = ScrollingFrame
+TextButton_16.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_16.BackgroundTransparency = 0.30000001192092896
+TextButton_16.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_16.BorderSizePixel = 0
+TextButton_16.Size = UDim2.new(0, 200, 0, 50)
+TextButton_16.Font = Enum.Font.Unknown
+TextButton_16.Text = "FE Fighter (R6)"
+TextButton_16.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_16.TextSize = 14
+local function onClick14()
+    loadstring(game:HttpGet("https://pastefy.app/wxVAgZpT/raw"))()
+end
+TextButton_16.MouseButton1Click:Connect(onClick14)
+
+UICorner_18.Parent = TextButton_16
+
+TextButton_17.Parent = ScrollingFrame
+TextButton_17.BackgroundColor3 = Color3.new(0, 0, 0)
+TextButton_17.BackgroundTransparency = 0.30000001192092896
+TextButton_17.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_17.BorderSizePixel = 0
+TextButton_17.Size = UDim2.new(0, 200, 0, 50)
+TextButton_17.Font = Enum.Font.Unknown
+TextButton_17.Text = "R6 Anims"
+TextButton_17.TextColor3 = Color3.new(0.0156863, 1, 0)
+TextButton_17.TextSize = 14
+local function onClick15()
+    loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Fe-Silly-animation-V4-16636"))()
+end
+TextButton_17.MouseButton1Click:Connect(onClick15)
+
+UICorner_19.Parent = TextButton_17
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel.BackgroundTransparency = 1
+ImageLabel.BorderColor3 = Color3.new(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Size = UDim2.new(0, 491, 0, 366)
+ImageLabel.ZIndex = 0
+ImageLabel.Image = "http://www.roblox.com/asset/?id=116380616656893"
+ImageLabel.ScaleType = Enum.ScaleType.Crop
+
+UICorner_20.Parent = ImageLabel
+
+UIPadding.Parent = ImageLabel
+
+UIPadding_2.Parent = Frame
+
+ImageLabel_2.Parent = Frame
+ImageLabel_2.BackgroundColor3 = Color3.new(0, 0, 0)
+ImageLabel_2.BackgroundTransparency = 0.4000000059604645
+ImageLabel_2.BorderColor3 = Color3.new(0, 0, 0)
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(-0.0122199589, 0, -0.0163934417, 0)
+ImageLabel_2.Size = UDim2.new(0, 504, 0, 378)
+ImageLabel_2.ZIndex = -1
+ImageLabel_2.Image = "http://www.roblox.com/asset/?id=116380616656893"
+ImageLabel_2.ImageTransparency = 1
+ImageLabel_2.ScaleType = Enum.ScaleType.Crop
+
+UICorner_21.Parent = ImageLabel_2
+
+UIPadding_3.Parent = ImageLabel_2
+
+ImageLabel_3.Parent = ScreenGui
+ImageLabel_3.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel_3.BorderColor3 = Color3.new(0, 0, 0)
+ImageLabel_3.BorderSizePixel = 0
+ImageLabel_3.Position = UDim2.new(0.717, 0, 0.203, 0)
+ImageLabel_3.Size = UDim2.new(0, 50, 0, 50)
+ImageLabel_3.Image = "http://www.roblox.com/asset/?id=2570751722"
+ImageLabel_3.MouseButton1Click:connect(function()
+	ScreenGui.Frame.Visible = not ScreenGui.Frame.Visible
+end)
+
+UICorner_22.Parent = ImageLabel_3
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.new(0, 0, 0)
+TextLabel.BackgroundTransparency = 0.800000011920929
+TextLabel.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(-0.0122199589, 0, 1.04918027, 0)
+TextLabel.Size = UDim2.new(0, 504, 0, 50)
+TextLabel.Font = Enum.Font.Sarpanch
+TextLabel.Text = "A-verage Hub"
+TextLabel.TextColor3 = Color3.new(0.133333, 1, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14
+TextLabel.TextStrokeColor3 = Color3.new(0, 1, 0.0313726)
+TextLabel.TextStrokeTransparency = 0
+TextLabel.TextTransparency = 0.8999999761581421
+TextLabel.TextWrapped = true
+
+UICorner_23.Parent = TextLabel
